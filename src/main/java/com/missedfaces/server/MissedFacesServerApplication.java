c4c9705831fc24d55f5d615b8c4ff9d3424609ab
Marcos Costa <costa.marcos.pro@gmail.com>
2016-02-29 20:39:20 -0300

    Commit inicial

---
+++ src/main/java/com/missedfaces/server/MissedFacesServerApplication.java
@@ -0,0 +1,12 @@
+package com.missedfaces.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissedFacesServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MissedFacesServerApplication.class, args);
+  }
+}
