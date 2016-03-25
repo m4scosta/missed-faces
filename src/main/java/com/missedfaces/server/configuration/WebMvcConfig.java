c09099a1f39c2a51674a24b6f46a05daf60b5ca8
Marcos Costa <costa.marcos.pro@gmail.com>
2016-03-25 01:26:55 -0300

    Adicionados templates da view

--- src/main/java/com/missedfaces/server/configuration/WebMvcConfig.java
+++ src/main/java/com/missedfaces/server/configuration/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.missedfaces.server.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,6 +18,12 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
   @Override
   public void addViewControllers(ViewControllerRegistry registry) {
     registry.addViewController("/").setViewName("index");
-    registry.addViewController("/login").setViewName("login");
+    registry.addViewController("/entrar").setViewName("login");
+    registry.addViewController("/cadastrarse").setViewName("signin");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
   }
 }
---
+++ src/main/resources/static/js/validation.js
@@ -0,0 +1,54 @@
+function getURLParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1));
+    var sURLVariables = sPageURL.split("&");
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
+
+function validateFieldsSignin($email, $password, $password2) {
+    validateEmail($email);
+    validatePassword($password);
+    validatePassword2($password2, $password);
+    return validateEmail($email) && validatePassword($password) && validatePassword2($password2, $password);
+}
+
+function validateFieldsLogin($email, $password) {
+    validateEmail($email);
+    validatePassword($password);
+    return validateEmail($email) && validatePassword($password);
+}
+
+function validateEmail($email) {
+    var isValid = $email.val();
+    if (!isValid) {
+        $email.parent().removeClass("has-success").addClass("has-error");
+    } else {
+        $email.parent().removeClass("has-error").addClass("has-success");
+    }
+    return isValid;
+}
+
+function validatePassword($password) {
+    var isValid = $password.val();
+    if (!isValid) {
+        $password.parent().removeClass("has-success").addClass("has-error");
+    } else {
+        $password.parent().removeClass("has-error").addClass("has-success");
+    }
+    return isValid;
+}
+
+function validatePassword2($password2, $password) {
+    var isValid = $password2.val() && $password2.val() == $password.val();
+    if (!isValid) {
+        $password2.parent().removeClass("has-success").addClass("has-error");
+    } else {
+        $password2.parent().removeClass("has-error").addClass("has-success");
+    }
+    return isValid;
+}
+
